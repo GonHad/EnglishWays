754f28601a2054cacccbdd0fdbb3a2de48f6962e
GonHad <gonzalohr.programacion@gmail.com>
2024-06-16 20:11:33 -0300

    Simplify database connection configuration

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-# app.py
-
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 from config import config
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 def get_db_connection():
     params = config()
