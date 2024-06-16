47c5c18562bda8aca8fd41b40e00e39e894ab91b
GonHad <gonzalohr.programacion@gmail.com>
2024-06-16 19:47:24 -0300

    Update app to listen on 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -54,6 +54,5 @@ def success():
     return render_template('success.html', email=email)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
